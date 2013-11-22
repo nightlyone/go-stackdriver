38a92a1f75b32f2e3e7fb0e645aa373b64235cc9
Ingo Oeser <nightlyone@googlemail.com>
2013-11-22 02:04:13 +0100

    fix json tag errors reported by go vet

--- stackdriver/annotation.go
+++ stackdriver/annotation.go
@@ -3,8 +3,8 @@ package stackdriver
 // an annotion made by someone
 type Annotation struct {
 	Message     string `json:"message"`
-	AnnotatedBy string `json:"annotated_by",omitempty`
-	Level       string `json:"level",omitempty`
-	IntanceId   string `json:"instance_id",omitempty`
-	EventEpoch  int64  `json:"event_epoch",omitempty`
+	AnnotatedBy string `json:"annotated_by,omitempty"`
+	Level       string `json:"level,omitempty"`
+	IntanceId   string `json:"instance_id,omitempty"`
+	EventEpoch  int64  `json:"event_epoch,omitempty"`
 }
--- stackdriver/deploy.go
+++ stackdriver/deploy.go
@@ -2,7 +2,7 @@ package stackdriver
 
 type Deploy struct {
 	RevisionId string `json:"revision_id"`
-	DeployedBy string `json:"deployed_by",omitempty`
-	DeployedTo string `json:"deployed_to",omitempty`
-	Repository string `json:"repository",omitempty`
+	DeployedBy string `json:"deployed_by,omitempty"`
+	DeployedTo string `json:"deployed_to,omitempty"`
+	Repository string `json:"repository,omitempty"`
 }
